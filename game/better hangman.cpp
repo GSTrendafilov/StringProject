e2d909783a32053b88813f5002b83dca87b7fa36
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2021-02-28 21:39:21 +0200

    replace old game with the new one

--- game/better hangman.cpp
+++ game/better hangman.cpp
@@ -1,130 +1,51 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string word,underscore,letter;
-int wrong=0;
-int main (){
-string copy = word;
-
-cout<<"Whats the word?"<<endl;
-
-cin >> word;
-
-
-for (int x=0; x<30; x++){
-cout << endl;
-}
-
-
-while (underscore.size() != word.size()){
-underscore.push_back('_');}
-
-cout << underscore << endl;
-
-while(wrong<12){
-cin >> letter;
-
-
-if(letter.size() > 1){
-if(letter==word){
-cout << "Thats the right word." << endl;
-break;
-}
-else{
-cout << underscore << endl;
-
-wrong ++;
-}
-
+void Victory_or_lost(string z, string x) {
+    if (z == x) {
+        cout << "Congrats, you win!";
     }
+    else {
+        cout << "Sorry, you lose!" << endl;
+        cout << "The word is >>" << x;
+    }
+}
+string checkingWord(string z, string x) {
+    unsigned maxguess(0);
+    char guess;
+
+    do
+    {
+        cout << "Uncovered: >>" << z << "<< Your guess: " << endl;
+        cin >> guess;
+        for (int i = 0; i < x.length(); i++)
+        {
+            if (guess == x[i])
+                z[i] = guess;
+            if (z == x) {
+                return "Congrats, you win!";
+            }
 
 
 
+        }
 
+    } while (++maxguess < x.size() + 5);
+    Victory_or_lost(z, x);
 
-if(underscore == word){
-  cout << "You win!" << endl;
-  break;
 }
 
+int main()
+{
 
-
-if(wrong==1){
-cout << "I" << endl;
-}
-else if(wrong==2){
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==3){
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==4){
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==5){
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==6){
-cout << "I===" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==7){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==8){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I  |" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==9){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I -|" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==10){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I -|-" << endl;
-cout << "I" << endl;
-cout << "I" << endl;
-}
-else if(wrong==11){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I -|-" << endl;
-cout << "I /"  << endl;
-cout << "I" << endl;
-}
-else if(wrong==12){
-cout << "I===" << endl;
-cout << "I  O" << endl;
-cout << "I -|-" << endl;
-cout << "I / /"<< endl;
-cout << "I YOU ARE DEAD" << endl;
-
-cout << "Game over bro! The word was: " << word <<endl;
-break;
-}
-}
+    string hidden;
+    cout << "-----------------------------HANGMAN-----------------------------" << endl;
+    cout << "Welcome to hangman, the game that every one knows how to play!" << endl;
+    cout << "What is the word for guessing?" << endl << "Type it here ------>";
+    cin >> hidden;
+    string answer;
+    for (int i = 0; i < hidden.size(); i++) {
+        answer = "*" + answer;
+    }
+    cout << checkingWord(answer, hidden);
 }
